3c386e4fdd580359c50fc6776dced13644f721fc
Python _data_processing <python_data_processing@python-data-processings-macbook-air.local>
2017-05-01 06:57:16 +0300

    Initial Commit

---
+++ Easy Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Tip
+//
+//  Created by Python_data_processing on 01/05/2017.
+//  Copyright © 2017 Python_data_processing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
