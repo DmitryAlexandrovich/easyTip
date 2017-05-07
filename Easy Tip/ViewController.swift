3e0b6b0179ac21e406f3f6ebc5178774543c35c7
Python_data_processing <python_data_processing@Python-data-processings-MacBook-Air.local>
2017-05-07 23:06:26 +0300

    Easy tip project has been added to repo

--- Easy Tip/ViewController.swift
+++ Easy Tip/ViewController.swift
@@ -1,25 +1,71 @@
-//
-//  ViewController.swift
-//  Easy Tip
-//
-//  Created by Python_data_processing on 01/05/2017.
-//  Copyright © 2017 Python_data_processing. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tipPercentageSlider: UISlider!
 
+    @IBOutlet weak var tipPercentageLable: UILabel!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    var tip = TipModel(billAmount: 0.0, tipPercent: 0.0)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setTipCalculatorValules()
+        updateUI()
+    }
+    
+    func setTipCalculatorValules() {
+    tip.tipPercent = Double (tipPercentageSlider.value)
+    tip.billAmount = ((textField.text)! as NSString).doubleValue
+    tip.calculateTip()
+    }
+    
+    func updateUI() {
+        tipLabel.text = String (format: "$%0.2f", tip.tipAmount)
+        totalLabel.text = String (format: "$%0.2f", tip.totalAmount) 
+        tipPercentageLable.text = "Tip: \(Int(tipPercentageSlider.value * 100))%"
+    }
+    
+    
+    @IBAction func billAmountDidChanged(_ sender: Any) {
+        setTipCalculatorValules()
+        updateUI()
+    }
+    
+    @IBAction func tipPercentageDidChange(_ sender: Any) {
+        setTipCalculatorValules()
+        updateUI()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
+
+    
+    
+    
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
